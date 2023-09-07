9cdfe3b21248e4ffc15b8f0ba307d9fbe1b3cafa
Antek Kutkowski <113889782+ant2oo7@users.noreply.github.com>
2023-09-07 11:04:29 +0300

    Add files via upload

---
+++ antek/jv.js
@@ -0,0 +1,16 @@
+// alert("Masz błąd sukoooooo");
+// confirm("Jesteś pewien??");
+// alert(confirm("Jesteś pewien??"));
+// prompt("Podaj liczbe","");
+// var nazwa_zmiennej;
+// let nazwa_zmiennej;
+// let name = prompt("Podaj imie", "");
+// alert(name)
+
+// = przypisanie
+// == równość 
+//=== identyczność
+let firstname = prompt("Podaj imie", "");
+let lastname = prompt("Podaj nazwisko", "");
+alert("Witaj " + firstname +" "+ lastname)
+
